2df35113dd22497e1887cd3ff22fbf1c493669fc
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2020-01-31 17:02:42 +0200

    Add 2.10 solutions

---
+++ 2-10-task2/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const officialJsName = prompt('What is the "official" name of JavaScript?', "");
+if (officialJsName === "ECMAScript") {
+  alert("Right!");
+} else {
+  alert("Didn't know? ECMAScript!");
+}
---
+++ 2-10-task3/script.js
@@ -0,0 +1,9 @@
+"use strict";
+const number = Number(prompt("Type a number", ""));
+if (number > 0) {
+  alert("1");
+} else if (number < 0) {
+  alert("-1");
+} else {
+  alert("0");
+}
